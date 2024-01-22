296a8f3d51b880ff6ff077a27b933aa2279a57ee
lopez-arch <156966233+lopez-arch@users.noreply.github.com>
2024-01-22 16:28:20 -0500

    Update app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+//variables
+let numerosecreto = 6;
+let numerousuario = prompt("me indicas un  numero por favor:");
+
+console.log(numerousuario);
+/*este codigo realiza
+ la comparacion
+ */
+if (numerousuario == numerosecreto) {
+    //acertamos, fue verdadera la condicion
+    alert("acertaste el numero");
+} else{
+    // la condicion no se cumplio
+    alert("lo siento, no acertaste el numero");
+}
